518d0541f9ef1f5659c5e50d4061edf9511b44ad
C-Vilen <101933465+C-Vilen@users.noreply.github.com>
2024-12-29 13:13:26 +0100

    Style: added style to start screen and input restriction

--- GuessMyNumberApp/views/StartGameScreen.js
+++ GuessMyNumberApp/views/StartGameScreen.js
@@ -1,15 +1,42 @@
-import { TextInput, View } from "react-native";
+import { TextInput, View, StyleSheet } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen () {
     return (
-        <View>
-            <TextInput />
+        <View style={styles.inputContainer}>
+            <TextInput maxLength={2} keyboardType="number-pad" style={styles.input}/>
             <PrimaryButton>Reset</PrimaryButton>
             <PrimaryButton>Confirm</PrimaryButton>
         </View>
     );
 }
 
-
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        padding: 16,
+        marginTop: 100,
+        marginHorizontal: 24,
+        backgroundColor: '#4e0329',
+        borderRadius: 8,
+        shadowColor: 'black',
+        shadowOffset: {
+            width: 4,
+            height: 4,
+        },
+        shadowRadius: 6,
+        shadowOpacity: 0.4,
+    },
+    input: {
+        height: 40,
+        width: 50,
+        fontSize: 32,
+        borderBottomColor: '#ddb52f',
+        borderBottomWidth: 1,
+        color: '#ddb52f',
+        marginVertical: 8,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+})
