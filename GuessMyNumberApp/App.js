7fd27decb140087049ee0323a9eaf81d40f5e47e
C-Vilen <101933465+C-Vilen@users.noreply.github.com>
2024-12-29 15:10:00 +0100

    Style: added background and image to start screen

--- GuessMyNumberApp/App.js
+++ GuessMyNumberApp/App.js
@@ -1,9 +1,29 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, ImageBackground } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
-import StartGameScreen from './views/StartGameScreen';
+import StartGameScreen from "./views/StartGameScreen";
 
 export default function App() {
-  return <StartGameScreen />;
+  return (
+    <LinearGradient
+      colors={["#04d32a", "#cbd5cd"]}
+      style={styles.rootContainer}>
+      <ImageBackground
+        source={require("./assets/images/background.png")}
+        resizeMode="cover"
+        style={styles.rootContainer}
+        imageStyle={styles.backgroundImage}>
+        <StartGameScreen />
+      </ImageBackground>
+    </LinearGradient>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+  },
+  backgroundImage:{
+    opacity: 0.2,
+  }
+});
