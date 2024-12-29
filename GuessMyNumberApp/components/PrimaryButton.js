f2fd32acee1e4b13c741878d4c4ed4ad6746daac
C-Vilen <101933465+C-Vilen@users.noreply.github.com>
2024-12-29 13:14:04 +0100

    Style: configured primary button and styled it

--- GuessMyNumberApp/components/PrimaryButton.js
+++ GuessMyNumberApp/components/PrimaryButton.js
@@ -1,11 +1,46 @@
-import { View, Text } from "react-native";
+import { View, Text, Pressable, StyleSheet } from "react-native";
 
-function PrimaryButton({children}) {
-    return (
-        <View>
-            <Text>{children}</Text>
-        </View>
-    );
+function PrimaryButton({ children }) {
+  function handlePress() {
+    console.log("Pressed");
+  }
+
+  return (
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        onPress={handlePress}
+        android_ripple={{ color: "#640233" }}
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.buttonPressed]
+            : styles.buttonInnerContainer
+        }>
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
+    </View>
+  );
 }
 
 export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+  },
+  buttonInnerContainer: {
+    backgroundColor: "#72063c",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 4,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+    textAlign: "center",
+  },
+  buttonPressed: {
+    opacity: 0.5,
+  },
+});
