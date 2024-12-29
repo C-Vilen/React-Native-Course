901b3b27c4bb6de69bb1549116578f093bd32841
C-Vilen <101933465+C-Vilen@users.noreply.github.com>
2024-12-29 17:00:31 +0100

    Feat: confirm button switch to game screen

--- GuessMyNumberApp/App.js
+++ GuessMyNumberApp/App.js
@@ -1,9 +1,22 @@
+import { useState } from "react";
 import { StyleSheet, ImageBackground } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 
 import StartGameScreen from "./views/StartGameScreen";
+import GameScreen from "./views/GameScreen";
 
 export default function App() {
+  const [userNumber, setUserNumber] = useState(undefined);
+
+  function startGameHandler(selectedNumber) {
+    setUserNumber(selectedNumber);
+  }
+
+  let screen = <StartGameScreen onSelectedNumber={startGameHandler} />;
+  if (userNumber) {
+    screen = <GameScreen />;
+  }
+
   return (
     <LinearGradient
       colors={["#04d32a", "#cbd5cd"]}
@@ -13,7 +26,7 @@ export default function App() {
         resizeMode="cover"
         style={styles.rootContainer}
         imageStyle={styles.backgroundImage}>
-        <StartGameScreen />
+        {screen}
       </ImageBackground>
     </LinearGradient>
   );
--- GuessMyNumberApp/views/StartGameScreen.js
+++ GuessMyNumberApp/views/StartGameScreen.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { TextInput, View, StyleSheet, Alert } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
-function StartGameScreen() {
+function StartGameScreen({ onSelectedNumber }) {
   const [enteredNumber, setEnteredNumber] = useState('');
 
   function numberInputHandler(inputText) {
@@ -19,7 +19,7 @@ function StartGameScreen() {
         Alert.alert('Invalid number', 'It must be a number between 1 and 99.', [{text: 'Okay', style: 'destructive', onPress: resetInputHandler}]);
         return;
     } 
-    console.log(chosenNumber + ' is a valid number');
+    onSelectedNumber(chosenNumber);
   }
   
   return (
