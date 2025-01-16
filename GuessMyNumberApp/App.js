0577e70f8efc1cc2f29b28b9f25d901c7a7710e2
C-Vilen <101933465+C-Vilen@users.noreply.github.com>
2025-01-16 20:47:41 +0100

    Feat: added statusbar to app.js

--- GuessMyNumberApp/App.js
+++ GuessMyNumberApp/App.js
@@ -4,6 +4,7 @@ import { LinearGradient } from "expo-linear-gradient";
 import Colors from "./constants/Colors";
 import { useFonts } from "expo-font";
 import AppLoading from "expo-app-loading";
+import { StatusBar } from "expo-status-bar";
 
 import StartGameScreen from "./views/StartGameScreen";
 import GameScreen from "./views/GameScreen";
@@ -56,19 +57,22 @@ export default function App() {
     }
 
     return (
-        <LinearGradient
-            colors={[Colors.lightGreen, Colors.whiteGreen]}
-            style={styles.rootContainer}>
-            <ImageBackground
-                source={require("./assets/images/background.png")}
-                resizeMode="cover"
-                style={styles.rootContainer}
-                imageStyle={styles.backgroundImage}>
-                <SafeAreaView style={styles.rootContainer}>
-                    {screen}
-                </SafeAreaView>
-            </ImageBackground>
-        </LinearGradient>
+        <>
+            <StatusBar style="light" />
+            <LinearGradient
+                colors={[Colors.lightGreen, Colors.whiteGreen]}
+                style={styles.rootContainer}>
+                <ImageBackground
+                    source={require("./assets/images/background.png")}
+                    resizeMode="cover"
+                    style={styles.rootContainer}
+                    imageStyle={styles.backgroundImage}>
+                    <SafeAreaView style={styles.rootContainer}>
+                        {screen}
+                    </SafeAreaView>
+                </ImageBackground>
+            </LinearGradient>
+        </>
     );
 }
 
