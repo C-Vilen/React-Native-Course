0c0fa894fa06b950df8d460d351babe5e12f7c85
C-Vilen <101933465+C-Vilen@users.noreply.github.com>
2025-01-19 10:55:21 +0100

    Feat: adde react navigator stack and implemented in App.js

--- MealRecipesApp/App.js
+++ MealRecipesApp/App.js
@@ -1,11 +1,19 @@
 import { StatusBar, StyleSheet, View } from "react-native";
 import CategoriesView from "./views/CategoriesView";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
 
 export default function App() {
     return (
         <View style={styles.container}>
           <StatusBar barStyle={"light-content"} />
-          <CategoriesView />
+          <NavigationContainer>
+            <Stack.Navigator>
+              <Stack.Screen name="Meal Categories" component={CategoriesView}/>
+            </Stack.Navigator>
+          </NavigationContainer>
         </View>
     );
 }
